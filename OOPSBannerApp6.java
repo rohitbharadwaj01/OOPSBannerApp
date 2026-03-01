8211a9f2bff5822e82f1779a1234f12a2763d96f
rohitbharadwaj01 <rb9858@srmist.edu.in>
2026-03-01 22:26:33 +0530

    Sixth Commit

---
+++ OOPSBannerApp6.java
@@ -0,0 +1,48 @@
+public class OOPSBannerApp6 {
+    public static void main(String[] args) {
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        // Render banner row by row
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(
+                    String.join("   ",
+                            O[i], O[i], P[i], S[i])
+            );
+        }
+    }
+    public static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+    public static String[] getP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+    public static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                "*     *",
+                " ***** "
+        };
+    }
+}
