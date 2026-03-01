7ee2236fa96466a22acb40b3bbe0ae13c822c379
rohitbharadwaj01 <rb9858@srmist.edu.in>
2026-03-01 20:27:50 +0530

    Third Commit

---
+++ OOPSBannerApp3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp3{
+	public static void main(String[]args){
+		System.out.println("OOPS");
+		OOPSBannerApp3 o1=new OOPSBannerApp3();
+		o1.UC3();
+	}
+	public void UC3(){
+		System.out.println(String.join("    ","*****","    ","*****","    ","*****","    ","*****"));
+		System.out.println(String.join("    ","*   *","    ","*   *","    ","*   *","    ","*    "));
+		System.out.println(String.join("    ","*   *","    ","*   *","    ","*****","    ","*****"));	
+		System.out.println(String.join("    ","*   *","    ","*   *","    ","*    ","    ","    *"));
+		System.out.println(String.join("    ","*   *","    ","*   *","    ","*    ","    ","    *"));
+		System.out.println(String.join("    ","*****","    ","*****","    ","*    ","    ","*****"));
+	}
+}
+	
